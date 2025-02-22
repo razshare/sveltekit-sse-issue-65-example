3f30b4a1421608f4c7f15e08e6136415c94cacbe
Razvan <razvan@razshare.dev>
2025-02-22 03:31:40 +0100

    chore(app): bump to 0.13.13.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,15 @@
   import { source } from 'sveltekit-sse';
 
   const connection = source('/events');
-  const stats = connection.select('message').json();
+  const stats = connection.select('message').json(function or() {
+    return { cpu_usage: '0.00%', memory: '0.00 MB', time: 0 };
+  });
 </script>
 
 <main>
   <h2>Admin dashboard page</h2>
 
-  {#if $stats}
-    <span>Cpu usage - {$stats.cpu_usage}</span><br />
-    <span>Memory - {$stats.memory}</span><br />
-    <span>Time - {$stats.time}</span><br />
-  {/if}
+  <span>Cpu usage - {$stats.cpu_usage}</span><br />
+  <span>Memory - {$stats.memory}</span><br />
+  <span>Time - {$stats.time}</span><br />
 </main>
