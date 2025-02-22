252c2ba37205dc1401b1080220624e8528f9b669
Razvan <razvan@razshare.dev>
2025-02-22 02:28:18 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { source } from 'sveltekit-sse';
+
+	const connection = source('/events');
+	const stats = connection.select('message').json();
+</script>
+
+<main>
+	<h2>Admin dashboard page</h2>
+
+	<pre>{JSON.stringify($stats)}</pre>
+</main>
